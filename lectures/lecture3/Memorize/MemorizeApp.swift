4afabac04caeb9d5cfd178516b6ab8ef364a329f
terryps <jterry9702@gmail.com>
2023-10-28 14:19:15 +0900

    Commit lecture3

---
+++ lectures/lecture3/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yeojin Jung on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
