277f8ff25d8d9c0183e6946440e5a9381eb9c527
terryps <jterry9702@gmail.com>
2023-10-28 14:34:14 +0900

    Removed count adjusters.

--- lectures/lecture3/Memorize/ContentView.swift
+++ lectures/lecture3/Memorize/ContentView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  let emojis: Array<String> = ["🦋", "🪺", "🏔️", "🦉", "🦊", "🐌", "🦎", "🍄"]
+  
+  
+  var body: some View {
+    ScrollView {
+      cards
     }
+    .padding()
+  }
+  
+  var cards: some View {
+    // LazyVGrid uses as little spaces as it can.
+    LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+      ForEach(emojis.indices, id: \.self) { i in
+        CardView(content: emojis[i], isFaceUp: true)
+          .aspectRatio(3/4, contentMode: .fit)
+      }
+    }
+    .foregroundColor(Color(hue: 0.442, saturation: 0.99, brightness: 0.985))
+  }
+}
+
+struct CardView: View {
+  let content: String
+  @State var isFaceUp: Bool = false
+  
+  var body: some View {
+    ZStack {
+      // The view is still struct.
+      let base: RoundedRectangle = RoundedRectangle(cornerRadius: 20)
+      
+      Group {
+        base.fill(.white)
+        base.strokeBorder(lineWidth: 4)
+        Text(content).font(.largeTitle).frame(maxHeight: .infinity)
+      }
+      base.fill().opacity(isFaceUp ? 0 : 1)
+    }.onTapGesture {
+      isFaceUp.toggle()
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
