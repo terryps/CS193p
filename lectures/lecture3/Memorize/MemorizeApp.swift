9eea2b993e7fc72756e75d95b6e8265f6b86dee7
terryps <jterry9702@gmail.com>
2023-11-01 15:33:22 +0900

    Publish view with view model

--- lectures/lecture3/Memorize/MemorizeApp.swift
+++ lectures/lecture3/Memorize/MemorizeApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct MemorizeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @StateObject var game = EmojiMemoryGame()
+  var body: some Scene {
+    WindowGroup {
+      EmojiMemoryGameView(gameViewModel: game)
     }
+  }
 }
