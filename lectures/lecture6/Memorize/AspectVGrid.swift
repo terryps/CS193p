af00c786b7598c217b660805be75e14ea4a590b0
terryps <jterry9702@gmail.com>
2023-11-28 22:28:31 +0900

    Updated AspectVGrid.

--- lectures/lecture6/Memorize/AspectVGrid.swift
+++ lectures/lecture6/Memorize/AspectVGrid.swift
@@ -7,14 +7,50 @@
 
 import SwiftUI
 
-struct AspectVGrid: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
+  var items: [Item]
+  var aspectRatio: CGFloat = 1
+  var content: (Item) -> ItemView // don't care type view
+  
+  init(_ items: [Item], aspectRatio: CGFloat, content: @escaping (Item) -> ItemView) {
+    self.items = items
+    self.aspectRatio = aspectRatio
+    self.content = content
+  }
+  
+  var body: some View {
+    GeometryReader { geometry in
+      let gridItemSize = gridItemWidthThatFits(
+        count: items.count,
+        size: geometry.size,
+        atAspectRatio: aspectRatio)
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+        ForEach(items) { item in
+          // Content is an function that takes an item and returns a View for it.
+          content(item)
+            .aspectRatio(aspectRatio, contentMode: .fit)
+        }
+      }
     }
-}
-
-struct AspectVGrid_Previews: PreviewProvider {
-    static var previews: some View {
-        AspectVGrid()
-    }
+  }
+  
+  func gridItemWidthThatFits(
+    count: Int,
+    size: CGSize,
+    atAspectRatio aspectRatio: CGFloat
+  ) -> CGFloat {
+    let count = CGFloat(count)
+    var columnCount = 1.0
+    repeat {
+      let width = size.width / columnCount
+      let height = width / aspectRatio
+      
+      let rowCount = (count / columnCount).rounded(.up)
+      if rowCount * height < size.height {
+        return (size.width / columnCount).rounded(.down)
+      }
+      columnCount += 1
+    } while columnCount < count
+    return min(size.width / count, size.height * aspectRatio.rounded(.down))
+  }
 }
--- lectures/lecture6/Memorize/EmojiMemoryGameView.swift
+++ lectures/lecture6/Memorize/EmojiMemoryGameView.swift
@@ -25,41 +25,15 @@ struct EmojiMemoryGameView: View {
   }
   
   private var cards: some View {
-    // LazyVGrid uses as little spaces as it can.
-    GeometryReader { geometry in
-      let gridItemSize = gridItemWidthThatFits(count: gameViewModel.cards.count, size: geometry.size, atAspectRatio: aspectRatio)
-      LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-        ForEach(gameViewModel.cards) { card in
-          CardView(card)
-            .aspectRatio(aspectRatio, contentMode: .fit)
-            .padding(4)
-            .onTapGesture {
-              gameViewModel.choose(card)
-            }
-        }
+    AspectVGrid(gameViewModel.cards, aspectRatio: aspectRatio) { card in
+      // Pass this view to AspectVGrid throung content argument.
+      return CardView(card)
+        .padding(4)
+        .onTapGesture {
+          gameViewModel.choose(card)
       }
     }
     .foregroundColor(Color(hue: 0.442, saturation: 0.99, brightness: 0.985))
-  }
-  
-  func gridItemWidthThatFits(
-    count: Int,
-    size: CGSize,
-    atAspectRatio aspectRatio: CGFloat
-  ) -> CGFloat {
-    let count = CGFloat(count)
-    var columnCount = 1.0
-    repeat {
-      let width = size.width / columnCount
-      let height = width / aspectRatio
-      
-      let rowCount = (count / columnCount).rounded(.up)
-      if rowCount * height < size.height {
-        return (size.width / columnCount).rounded(.down)
-      }
-      columnCount += 1
-    } while columnCount < count
-    return min(size.width / count, size.height * aspectRatio.rounded(.down))
   }
 }
 
