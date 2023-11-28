bfca3e395c23d64cfa632873ab6cb00d467b8078
terryps <jterry9702@gmail.com>
2023-11-28 21:33:58 +0900

    Commit AspectVGrid.swift

---
+++ lectures/lecture6/Memorize/AspectVGrid.swift
@@ -0,0 +1,20 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Yeojin Jung on 2023/11/28.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AspectVGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        AspectVGrid()
+    }
+}
