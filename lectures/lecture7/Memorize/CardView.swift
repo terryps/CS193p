5bc92f9070c70a32b735d9c810d97295c76ec1a2
terryps <jterry9702@gmail.com>
2023-12-01 13:11:24 +0900

    Created CardView.swift

---
+++ lectures/lecture7/Memorize/CardView.swift
@@ -0,0 +1,40 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Yeojin Jung on 2023/12/01.
+//
+
+import SwiftUI
+
+struct CardView: View {
+  let card: MemoryGame<String>.Card
+  
+  init(_ card: MemoryGame<String>.Card) {
+    self.card = card
+  }
+  
+  var body: some View {
+    ZStack {
+      // The view is still struct.
+      let base: RoundedRectangle = RoundedRectangle(cornerRadius: 20)
+      
+      Group {
+        base.fill(.white)
+        base.strokeBorder(lineWidth: 4)
+        Text(card.content)
+          .font(.system(size: 200))
+          .minimumScaleFactor(0.01)
+          .aspectRatio(1, contentMode: .fit)
+      }
+      // .opacity(card.isFaceUp ? 1 : 0)
+      base.fill().opacity(card.isFaceUp || card.isMatched ? 0 : 1)
+    }
+  }
+}
+
+struct CardView_Previews: PreviewProvider {
+  static var previews: some View {
+    CardView(MemoryGame<String>.Card(content: "X", id: "test1"))
+  }
+}
--- lectures/lecture7/Memorize/EmojiMemoryGameView.swift
+++ lectures/lecture7/Memorize/EmojiMemoryGameView.swift
@@ -37,31 +37,6 @@ struct EmojiMemoryGameView: View {
   }
 }
 
-struct CardView: View {
-  let card: MemoryGame<String>.Card
-  
-  init(_ card: MemoryGame<String>.Card) {
-    self.card = card
-  }
-  
-  var body: some View {
-    ZStack {
-      // The view is still struct.
-      let base: RoundedRectangle = RoundedRectangle(cornerRadius: 20)
-      
-      Group {
-        base.fill(.white)
-        base.strokeBorder(lineWidth: 4)
-        Text(card.content)
-          .font(.system(size: 200))
-          .minimumScaleFactor(0.01)
-          .aspectRatio(1, contentMode: .fit)
-      }
-      // .opacity(card.isFaceUp ? 1 : 0)
-      base.fill().opacity(card.isFaceUp || card.isMatched ? 0 : 1)
-    }
-  }
-}
 
 struct EmojiMemoryGameView_Previews: PreviewProvider {
     static var previews: some View {
