63315c99502fe0d2e4450d68505dbdecc44f9db3
terryps <jterry9702@gmail.com>
2023-12-11 16:12:12 +0900

    Commit FlyingNumber.swift

---
+++ lectures/lecture8/Memorize/FlyingNumber.swift
@@ -0,0 +1,24 @@
+//
+//  FlyingNumber.swift
+//  Memorize
+//
+//  Created by Yeojin Jung on 2023/12/11.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+  let number: Int
+  
+    var body: some View {
+      if number != 0 {
+        Text(number, format: .number)
+      }
+    }
+}
+
+struct FlyingNumber_Previews: PreviewProvider {
+    static var previews: some View {
+      FlyingNumber(number: 5)
+    }
+}
