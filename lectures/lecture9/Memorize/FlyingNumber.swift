4b969f2ac2cc5b3003cd2b1f22b81a4e78ba936f
terryps <jterry9702@gmail.com>
2023-12-13 18:23:44 +0900

    Updated FlyNumber to make score text clearly visible   • font - .largeTitle   • foregroundColor   • shadow

--- lectures/lecture9/Memorize/FlyingNumber.swift
+++ lectures/lecture9/Memorize/FlyingNumber.swift
@@ -12,7 +12,10 @@ struct FlyingNumber: View {
   
     var body: some View {
       if number != 0 {
-        Text(number, format: .number)
+        Text(number, format: .number.sign(strategy: .always()))
+          .font(.largeTitle)
+          .foregroundColor(number < 0 ? .red : .blue)
+          .shadow(color: .black, radius: 1.5, x: 1, y: 1)
       }
     }
 }
