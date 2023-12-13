8e4b18057e6f7bb8dc57bbbfc46e0f58a4ba961f
terryps <jterry9702@gmail.com>
2023-12-13 18:32:48 +0900

    Added animations for opacity and offset to Flying Number

--- lectures/lecture9/Memorize/FlyingNumber.swift
+++ lectures/lecture9/Memorize/FlyingNumber.swift
@@ -10,14 +10,23 @@ import SwiftUI
 struct FlyingNumber: View {
   let number: Int
   
-    var body: some View {
-      if number != 0 {
-        Text(number, format: .number.sign(strategy: .always()))
-          .font(.largeTitle)
-          .foregroundColor(number < 0 ? .red : .blue)
-          .shadow(color: .black, radius: 1.5, x: 1, y: 1)
-      }
+  @State private var offset: CGFloat = 0
+  
+  var body: some View {
+    if number != 0 {
+      Text(number, format: .number.sign(strategy: .always()))
+        .font(.largeTitle)
+        .foregroundColor(number < 0 ? .red : .blue)
+        .shadow(color: .black, radius: 1.5, x: 1, y: 1)
+        .offset(x: 0, y: offset)
+        .opacity(offset != 0 ? 0 : 1)
+        .onAppear {
+          withAnimation(.easeIn(duration: 1.5)) {
+            offset = number < 0 ? 200 : -200
+          }
+        }
     }
+  }
 }
 
 struct FlyingNumber_Previews: PreviewProvider {
