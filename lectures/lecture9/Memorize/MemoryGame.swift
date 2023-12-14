5246a10f25630f507e9ee011bda7bd08b8c6c626
terryps <jterry9702@gmail.com>
2023-12-14 19:21:15 +0900

    Updated Game model   • bonus time rule for Card

--- lectures/lecture9/Memorize/MemoryGame.swift
+++ lectures/lecture9/Memorize/MemoryGame.swift
@@ -80,6 +80,51 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     var debugDescription: String {
       return "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? "matched" : "")"
     }
+    
+    // MARK: - Bonus Time
+    
+    // can be zero which would mean "no bonus available" for matching this card quickly
+    var bonusTimeLimit: TimeInterval = 6
+    
+    // the last time this card was turned face up
+    var lastFaceUpDate: Date?
+    
+    // the accumulated time this card was face up in the past
+    // (i.e. not including the current time it's been face up if it is currently so)
+    var pastFaceUpTime: TimeInterval = 0
+    
+    // how long this card has ever been face up and unmatched during its lifetime
+    // basically, pastFaceUpTime + time since lastFaceUpDate
+    var faceUpTime: TimeInterval {
+      if let lastFaceUpDate {
+        return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+      } else {
+        return pastFaceUpTime
+      }
+    }
+    
+    // the bonus earned so far (one point for every second of the bonusTimeLimit that was not used)
+    // this gets smaller and smaller the longer the card remains face up without being matched
+    var bonus: Int {
+        Int(bonusTimeLimit * bonusPercentRemaining)
+    }
+    
+    // percentage of the bonus time remaining
+    var bonusPercentRemaining: Double {
+        bonusTimeLimit > 0 ? max(0, bonusTimeLimit - faceUpTime)/bonusTimeLimit : 0
+    }
+    
+    // call this when the card transitions to face up state
+    private mutating func startUsingBonusTime() {
+      if isFaceUp && !isMatched && bonusPercentRemaining > 0, lastFaceUpDate == nil {
+        lastFaceUpDate = Date()
+      }
+    }
+    
+    private mutating func stopUsingBonusTime() {
+      pastFaceUpTime = faceUpTime
+      lastFaceUpDate = nil
+    }
   }
 }
 
