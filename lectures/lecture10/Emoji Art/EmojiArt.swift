dd96a1e5c91b7ea49b0cf63c421ffec4359ed859
terryps <jterry9702@gmail.com>
2023-12-19 18:08:54 +0900

    Commit lecture10

---
+++ lectures/lecture10/Emoji Art/EmojiArt.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiArt.swift
+//  Emoji Art
+//
+//  Created by Yeojin Jung on 2023/12/19.
+//
+
+import Foundation
---
+++ lectures/lecture10/Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Yeojin Jung on 2023/12/19.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
