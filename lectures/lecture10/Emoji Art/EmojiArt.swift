9ee28b4b6347e9d995746052cfa16aee850ffa68
terryps <jterry9702@gmail.com>
2023-12-20 15:36:16 +0900

    Added an Identifiable protocol to struct Emoji to make it able to use ForEach.

--- lectures/lecture10/Emoji Art/EmojiArt.swift
+++ lectures/lecture10/Emoji Art/EmojiArt.swift
@@ -5,16 +5,33 @@
 //  Created by Yeojin Jung on 2023/12/19.
 //
 
+// MVVM - model
+
 import Foundation
 
 struct EmojiArt {
   var background: URL? = nil
-  var emojis = [Emoji]()
+  private(set) var emojis = [Emoji]()
+  
+  private var uniqueEmojiId = 0
+  
+  mutating func addEmoji(_ emoji: String, at position: Emoji.Position, size: Int) {
+    uniqueEmojiId += 1
+    emojis.append(Emoji(
+      string: emoji,
+      position: position,
+      size: size,
+      id: uniqueEmojiId
+    ))
+  }
 
-  struct Emoji {
+  struct Emoji: Identifiable {
+    // Emoji has to stay. Once its content decided, it doesn't change to something else.
+    // So use let.
     let string: String
     var position: Position
     var size: Int
+    var id: Int
     
     struct Position {
       var x: Int
