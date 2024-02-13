9d7f82537fd54a753a771d4c70237f14c97fbfcd
terryps <jterry9702@gmail.com>
2024-02-13 18:25:50 +0900

    feat: add paletteStore and pass down through .environmentObject

--- lectures/lecture11/Emoji Art/Emoji_ArtApp.swift
+++ lectures/lecture11/Emoji Art/Emoji_ArtApp.swift
@@ -10,10 +10,14 @@ import SwiftUI
 @main
 struct Emoji_ArtApp: App {
   @StateObject var defaultDocument = EmojiArtDocument()
+  @StateObject var paletteStore = PaletteStore(named: "Main")
   
     var body: some Scene {
         WindowGroup {
             EmojiArtDocumentView(document: defaultDocument)
+              // Get a thing of type PaletteStore that was injected into me.
+              // This parameter only works when you hav one thing of a certain type that's passed down into a view hierarchy.
+              .environmentObject(paletteStore)
         }
     }
 }
