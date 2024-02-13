89e4b23759a28d6e127f6a06d3b0353f8ca03abe
terryps <jterry9702@gmail.com>
2024-02-13 19:11:33 +0900

    feat: add contextMenu to Palette Button

--- lectures/lecture11/Emoji Art/PaletteChooser.swift
+++ lectures/lecture11/Emoji Art/PaletteChooser.swift
@@ -21,12 +21,16 @@ struct PaletteChooser: View {
     }
   
   var chooser: some View {
-    Button {
-      withAnimation {
-        store.cursorIndex += 1
+    AnimatedActionButton(systemImage: "paintpalette") {
+      store.cursorIndex += 1
+    }
+    .contextMenu {
+      AnimatedActionButton("New", systemImage: "plus") {
+        store.insert(name: "Math", emojis: "+-x%=")
       }
-    } label: {
-      Image(systemName: "paintpalette")
+      AnimatedActionButton("Delete", systemImage: "minus") {
+        store.palettes.remove(at: store.cursorIndex)
+      }
     }
   }
   
--- lectures/lecture11/Emoji Art/PaletteStore.swift
+++ lectures/lecture11/Emoji Art/PaletteStore.swift
@@ -41,4 +41,42 @@ class PaletteStore: ObservableObject {
     }
     return index
   }
+  
+  // MARK: Adding Palettes
+  
+  // these functions are the recommended way to add Palettes to the PaletteStore
+  // since they try to avoid duplication of Identifiable-ly identical Palettes
+  // by first removing/replacing any Palette with the same id that is already in palettes
+  // it does not "remedy" existing duplication, it just does not "cause" new duplication
+  
+  func insert(_ palette: Palette, at insertionIndex: Int? = nil) { // "at" default is cursorIndex
+    let insertionIndex = boundsCheckedPaletteIndex(insertionIndex ?? cursorIndex)
+    if let index = palettes.firstIndex(where: { $0.id == palette.id }) {
+      palettes.move(fromOffsets: IndexSet([index]), toOffset: insertionIndex)
+      palettes.replaceSubrange(insertionIndex...insertionIndex, with: [palette])
+    } else {
+      palettes.insert(palette, at: insertionIndex)
+    }
+  }
+  
+  func insert(name: String, emojis: String, at index: Int? = nil) {
+    insert(Palette(name: name, emojis: emojis), at: index)
+  }
+  
+  func append(_ palette: Palette) { // at end of palettes
+    if let index = palettes.firstIndex(where: { $0.id == palette.id }) {
+      if palettes.count == 1 {
+        palettes = [palette]
+      } else {
+        palettes.remove(at: index)
+        palettes.append(palette)
+      }
+    } else {
+      palettes.append(palette)
+    }
+  }
+  
+  func append(name: String, emojis: String) {
+    append(Palette(name: name, emojis: emojis))
+  }
 }
