cdfb04cab65e84303bb11ac43b02baea70e57598
terryps <jterry9702@gmail.com>
2024-02-16 15:04:22 +0900

    feat: create PaletteEditor View   • Form, Vstack-like thing, is used when you collect information from the user.   • Section is a container View used to add hierarchy within certain views.

--- lectures/lecture13/Emoji Art/PaletteEditor.swift
+++ lectures/lecture13/Emoji Art/PaletteEditor.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct PaletteEditor: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  let palette: Palette
+  
+  var body: some View {
+    // Form is an extremely powerful VStack-like thing
+    // for when you want to collect information from the user.
+    Form {
+      Section(header: Text("Name")) {
+        Text(palette.name)
+      }
+      Section(header: Text("Emojis")) {
+        Text("Add Emojis Here")
+        removeEmojis
+      }
     }
+    .frame(minWidth: 300, minHeight: 350)
+  }
+  
+  private var removeEmojis: some View {
+    VStack(alignment: .trailing) {
+      Text("Tap to Remove Emojis").font(.caption).foregroundColor(.gray)
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+        ForEach(palette.emojis.uniqued.map(String.init), id: \.self) { emoji in
+          Text(emoji)
+        }
+      }
+    }
+  }
 }
 
-#Preview {
-    PaletteEditor()
-}
+//#Preview {
+//    PaletteEditor()
+//}
