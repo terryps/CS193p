e1fd13fc71b0f862614deb6fce422426159f1445
terryps <jterry9702@gmail.com>
2024-02-21 12:24:38 +0900

    feat: add FocusState focused to keep track of which of the TextFields has the focus

--- lectures/lecture13/Emoji Art/PaletteEditor.swift
+++ lectures/lecture13/Emoji Art/PaletteEditor.swift
@@ -14,15 +14,30 @@ struct PaletteEditor: View {
   
   @State private var emojisToAdd: String = ""
   
+  enum Focused {
+    case name
+    case addEmojis
+  }
+  
+  // This state keeps track of which of the TextFields has the focus of the keyboard.
+  // Oftentimes, this is a bool because you'll only have one TextField and it's either focus or not.
+  // Since we have two TextFields, make focused be little Enum.
+  // It has to be optional because neither of them might be focused.
+  @FocusState private var focused: Focused?
+  
   var body: some View {
     // Form is an extremely powerful VStack-like thing
     // for when you want to collect information from the user.
     Form {
       Section(header: Text("Name")) {
         TextField("Name", text: $palette.name)
+        // Give TextField a binding to this little FocusState thing like that
+        // and you tell it here it is name.
+          .focused($focused, equals: .name)
       }
       Section(header: Text("Emojis")) {
         TextField("Add Emojis Here", text: $emojisToAdd)
+          .focused($focused, equals: .addEmojis)
           .font(emojiFont)
           .onChange(of: emojisToAdd) {
             // This palette is in ViewModel. In other words, I'm changing the ViewModel right here.
@@ -34,6 +49,13 @@ struct PaletteEditor: View {
       }
     }
     .frame(minWidth: 300, minHeight: 350)
+    .onAppear {
+      if palette.name.isEmpty {
+        focused = .name
+      } else {
+        focused = .addEmojis
+      }
+    }
   }
   
   private var removeEmojis: some View {
